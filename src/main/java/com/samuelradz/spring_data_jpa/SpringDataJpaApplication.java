fed045597ff712e69933a69748bfec9832a76891
Samuel Raducan <samuel.thembp@gmail.com>
2024-07-30 18:09:07 +0200

    Add Author entity with JPA annotations

--- src/main/java/com/samuelradz/spring_data_jpa/SpringDataJpaApplication.java
+++ src/main/java/com/samuelradz/spring_data_jpa/SpringDataJpaApplication.java
@@ -1,5 +1,6 @@
 package com.samuelradz.spring_data_jpa;
 
+import com.samuelradz.spring_data_jpa.models.Author;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
---
+++ src/main/java/com/samuelradz/spring_data_jpa/models/Author.java
@@ -0,0 +1,28 @@
+package com.samuelradz.spring_data_jpa.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "author_sequence"
+    )
+    @SequenceGenerator(
+            name = "author_sequence",
+            sequenceName = "author_sequence",
+            allocationSize = 1
+    )
+    private Integer id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private int age;
+}
