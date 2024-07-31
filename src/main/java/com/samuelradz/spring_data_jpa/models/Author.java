c44d5e81cc5f1bb5d468f5772bfc861936a3b3b7
Samuel Raducan <samuel.thembp@gmail.com>
2024-07-31 19:06:46 +0200

    JPA: Working with @Column annotation

--- src/main/java/com/samuelradz/spring_data_jpa/models/Author.java
+++ src/main/java/com/samuelradz/spring_data_jpa/models/Author.java
@@ -4,25 +4,64 @@ import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @NoArgsConstructor
 @Data
 @Entity
 public class Author {
 
     @Id
+    // DEFAULT: strategy = GenerationType.AUTO
     @GeneratedValue(
-            strategy = GenerationType.SEQUENCE,
-            generator = "author_sequence"
+//            strategy = GenerationType.SEQUENCE,
+//            strategy = GenerationType.TABLE,
+//            generator = "author_sequence"
+//            generator = "author_id_gen"
     )
-    @SequenceGenerator(
-            name = "author_sequence",
-            sequenceName = "author_sequence",
-            allocationSize = 1
-    )
+
+    // Default is author_seq
+    // @SequenceGenerator can customise the naming
+//    @SequenceGenerator(
+//            name = "author_sequence",
+//            sequenceName = "author_sequence",
+//            allocationSize = 1
+//    )
+//    @TableGenerator(
+//            name = "author_id_gen", // author
+//            table = "id_generator",
+//            pkColumnName = "id_name", // author
+//            valueColumnName = "id_value",
+//            allocationSize = 1
+//    )
     private Integer id;
 
+    @Column(
+            name = "f_name",
+            length = 35
+    )
     private String firstName;
+
     private String lastName;
+
+    // Control over tables / entities in the db
+    @Column(
+            unique = true,
+            nullable = false
+    )
     private String email;
+
     private int age;
+
+    @Column(
+            updatable = false,
+            nullable = false
+    )
+    private LocalDateTime createdAt;
+
+    @Column(
+            insertable = false
+    )
+    private LocalDateTime lastModified;
+
 }
