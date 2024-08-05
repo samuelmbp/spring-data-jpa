15fb0382dd66e5e3d60dec69cb7eb8b113a05f20
Samuel Raducan <samuel.thembp@gmail.com>
2024-08-05 11:24:08 +0200

    Added one to many relationship between course and section

--- src/main/java/com/samuelradz/spring_data_jpa/models/Section.java
+++ src/main/java/com/samuelradz/spring_data_jpa/models/Section.java
@@ -1,8 +1,6 @@
 package com.samuelradz.spring_data_jpa.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -21,4 +19,8 @@ public class Section {
 
     private String name;
     private int sectionOrder;
+
+    @ManyToOne
+    @JoinColumn(name = "course_id")
+    private Course course;
 }
