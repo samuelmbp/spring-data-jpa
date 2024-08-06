91009122f5a7d2a2feb2638271627ace5ab4af6f
Samuel Raducan <samuel.thembp@gmail.com>
2024-08-06 09:00:31 +0200

    Added one to one relationship between lecture and resource

--- src/main/java/com/samuelradz/spring_data_jpa/models/Resource.java
+++ src/main/java/com/samuelradz/spring_data_jpa/models/Resource.java
@@ -1,8 +1,6 @@
 package com.samuelradz.spring_data_jpa.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -22,4 +20,9 @@ public class Resource {
     private String name;
     private int size;
     private String url;
+
+
+    @OneToOne
+    @JoinColumn(name = "lecture_id")
+    private Lecture lecture;
 }
